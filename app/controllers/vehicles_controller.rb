7ff9c93824da6173fcfd52decb955a86f097c005
Larry Nesbit <larryjnesbit@gmail.com>
2018-01-25 04:25:24 -0600

    end of exercise 14

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -6,4 +6,37 @@ class VehiclesController < ApplicationController
 	def show
 		@vehicles =Vehicle.find(params[:id])
 	end
+
+	def edit
+		 @vehicles = Vehicle.find(params[:id])
+	end
+
+	def update
+		@vehicles = Vehicle.find(params[:id])
+		@vehicles.update(vehicles_params)
+		redirect_to @vehicles		
+	end
+
+	def new
+		@vehicles = Vehicle.new
+	end
+
+	def create
+		@vehicles=Vehicle.new(vehicles_params)
+		@vehicles.save
+		redirect_to @vehicles		
+	end
+
+	def destroy
+		@vehicles = Vehicle.find(params[:id])
+		@vehicles.destroy
+		redirect_to vehicles_url
+		
+	end
+
+private
+	def vehicles_params
+		params.require(:vehicle).
+			permit(:make, :model, :year, :price, :features)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'vehicles#index'
-	get 'vehicles' => 'vehicles#index'
-	get 'vehicles/:id' => 'vehicles#show', as: "vehicle"
+	root 'vehicles#index'
+
+
+	resources :vehicles
+
+	#get 'vehicles' => 'vehicles#index'
+	#get 'vehicles/:id' => 'vehicles#show', as: "vehicle"
 end
